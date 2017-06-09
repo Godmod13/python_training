66727d4d5a4ca75395e803801edb710cafb8cae5
Godmod13 <s1874@mail.ru>
2017-06-09 14:00:39 +0500

    Добавлена оптимизация добавление групп

--- fixture/session.py
+++ fixture/session.py
@@ -8,13 +8,14 @@ class SessionHelper:
     def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
+        time.sleep(1)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
         time.sleep(1)
 
     def logout(self):
@@ -22,19 +23,15 @@ class SessionHelper:
         time.sleep(1)
         wd.find_element_by_link_text("Logout").click()
 
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
 
     def ensure_logout(self):
         wd = self.app.wd
         if self.is_logged_in():
             self.logout()
-
-    def is_logged_in(self):
-        wd = self.app.wd
-        return len(wd.find_elements_by_link_text("Logout")) > 0
-
-    def is_logged_in_as(self, username):
-        wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
 
 
     def ensure_login(self, username, password):
@@ -45,3 +42,7 @@ class SessionHelper:
             else:
                 self.logout()
         self.login(username, password)
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,10 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
-        app.group.create(Group(name="tgtgtg", header="tgtgt", footer="gtgtg"))
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="tgtgtg", header="tgtgt", footer="gtgtg", id=None))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
-        app.group.create(Group(name="", header="", footer=""))
-
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="", header="", footer="", id= None))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
